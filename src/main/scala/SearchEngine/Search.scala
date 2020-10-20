37a3783f9f764d96b689f62fe23cf95d3d4574bb
Angel Igareta <angel@igareta.com>
2020-10-21 00:17:42 +0200

    Ref: Grouping places into cities to work both with airport and bus stations

--- src/main/scala/SearchEngine/Search.scala
+++ src/main/scala/SearchEngine/Search.scala
@@ -1,5 +1,6 @@
 package SearchEngine
 
+import SearchEngine.Search.TransportType.TransportType
 import kafka.serializer.StringDecoder
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.SparkConf
@@ -44,12 +45,12 @@ object Search {
     // Subscribe to topic using second approach => [Direct Stream](https://medium.com/@rinu.gour123/apache-kafka-spark-streaming-integration-af7bd87887fb
     val topicStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConsumerParams, Set(topicName))
 
+    // Listen to incoming events of input travel topic
     topicStream.foreachRDD(record => {
       record.collect().foreach((inputRow) => {
         processTrip(inputRow)
       })
     })
-
 
     // Start streaming job
     ssc.start()
@@ -58,69 +59,41 @@ object Search {
     ssc.awaitTermination()
   }
 
-  private def processTrip(inputRow: (String, String)) = {
+  private def processTrip(inputRow: (String, String)): Unit = {
     println(s"Raw ${inputRow._2}")
     val travelQueryInputJson = JSON.parseFull(inputRow._2)
 
     travelQueryInputJson match {
       // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
       case Some(travelQueryInput: Map[String, Any]) => {
+        // Get variables from travel query
         val src = travelQueryInput.getOrElse("src", "1, 1").toString
         val dst = travelQueryInput.getOrElse("dst", "1, 1").toString
 
-        // Get nearby places
-        val srcNearbyPlaces: Set[Place] = getNearbyPlacesFromCityRaw(src)
-        val dstNearbyPlaces: Set[Place] = getNearbyPlacesFromCityRaw(dst)
+        // Get nearby cities from src and destination [So they can be used as transit stops to make the trip cheaper]
+        val srcCities: List[City] = getCities(src)
+        val dstCities: List[City] = getCities(dst)
 
-        val srcPlaces: Set[Place] = srcNearbyPlaces.filter(!_.isTransit)
-        val dstPlaces: Set[Place] = dstNearbyPlaces.filter(!_.isTransit)
-        val transitPlaces: Set[Place] = srcNearbyPlaces.filter(_.isTransit) ++ dstNearbyPlaces.filter(_.isTransit)
+        /**
+         * Divide found places into three categories:
+         * 1. Origin City: City closer to the origin coordinates.
+         * 2. Destination Places: City closer to the destination coordinates.
+         * 3. Transit Places: The ones that were found near origin or destination but do not meet previous criteria
+         */
+        val originCity: City = srcCities.find(!_.isTransit).orNull
+        val destinationCity: City = dstCities.find(!_.isTransit).orNull
+        val transitCities: List[City] = srcCities.filter(_.isTransit) ++ dstCities.filter(_.isTransit)
 
-        // Get possibles routes from each src to each dst
-        var possiblePlacesCombinations: Array[Array[Place]] = Array()
-        srcPlaces.foreach(srcPlace => {
-          dstPlaces.foreach(dstPlace => {
-            transitPlaces.foreach(transitPlace => {
-              possiblePlacesCombinations = possiblePlacesCombinations :+ Array(srcPlace, transitPlace, dstPlace)
-            })
+        // Create graph. In its constructor it will use transport APIs to create the edges between the city nodes.
 
-            // Also direct combinations
-            possiblePlacesCombinations = possiblePlacesCombinations :+ Array(srcPlace, dstPlace)
-          })
-        })
-
-        println("Possible combinations")
-        possiblePlacesCombinations.foreach(possiblePlacesCombination => {
-          println(possiblePlacesCombination.mkString("Array(", ", ", ")"))
-        })
-
-        // For each combination look for routes
-        // Flights
-
-        // TODO: Transport
       }
       case None => println("Parsing failed")
       case other => println("Unknown data structure: " + other)
     }
   }
 
-  private def getNearbyPlacesFromCityRaw (city: String): Set[Place] = {
-    var srcNearbyPlaces: Set[Place] = Set()
-
-    val srcNearbyCities = getNearbyPlacesFromCity(city)
-    srcNearbyCities.foreach(city => {
-      val srcNearbySkyPlaces = getNearbyStations(city)
-      // println(s"Places close to city ${city}: ${srcNearbySkyPlaces}")
-      srcNearbySkyPlaces.foreach(place => {
-        srcNearbyPlaces += place
-      })
-    })
-
-    srcNearbyPlaces
-  }
-
-  def getNearbyPlacesFromCity(location: String): List[City] = {
-    val nearbyCityRequest = Http(s"https://rapidapi.p.rapidapi.com/v1/geo/locations/${location}/nearbyCities")
+  def getCities(coordinates: String): List[City] = {
+    val nearbyCityRequest = Http(s"https://rapidapi.p.rapidapi.com/v1/geo/locations/${coordinates}/nearbyCities")
       .param("radius", geoDbRadius.toString)
       .param("limit", geoDbLimit.toString)
       .param("minPopulation", geoDbMinPopulation.toString)
@@ -137,17 +110,22 @@ object Search {
     response match {
       // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
       case Some(nearbyCitiesQueryRaw: Map[String, List[Map[String, Any]]]) =>
-        val nearbyCities = nearbyCitiesQueryRaw.getOrElse("data", List())
-        val smallestDistance = if (nearbyCities.nonEmpty) nearbyCities.head.getOrElse("distance", Double.MaxValue).asInstanceOf[Double] else 0.0
+        val nearbyCitiesRaw = nearbyCitiesQueryRaw.getOrElse("data", List())
 
-        val nearbyCitiesNames = nearbyCities.map(city => {
+        // Get information from cities from GeoDB and fill with places retrieved from transport APIs [Sorted by distance]
+        val nearbyCities = nearbyCitiesRaw.zipWithIndex.map{case (city, index) => {
           val cityName = city.getOrElse("city", "").toString
           val cityCountryCode = city.getOrElse("countryCode", "").toString
-          val cityDistance = city.getOrElse("distance", Double.MaxValue).asInstanceOf[Double]
+          // val cityDistance = city.getOrElse("distance", Double.MaxValue).asInstanceOf[Double]
 
-          City(cityName, cityCountryCode, isTransit = (cityDistance > smallestDistance + thresholdDistance))
-        })
-        nearbyCitiesNames
+          val newCity = City(cityName, cityCountryCode, index != 0, Set()) // isTransit = cityDistance > smallestDistance + thresholdDistance
+
+          // Fill with places from transport APIs
+          newCity.places ++ getPlaces(newCity)
+          newCity
+        }}
+
+        nearbyCities
       case None =>
         println("Parsing failed")
         List()
@@ -158,15 +136,21 @@ object Search {
   }
 
 
-  def getNearbyStations(city: City): List[Place] = {
-    //    val nearbyCityRequest = Http(s"https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/${city.countryCode}/EUR/en-GB/")
-    //      .param("query", city.name)
-    //      .header("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com")
-    //      .header("x-rapidapi-key", "c1d6dfff64msh11b436d0b19f66cp1aa76bjsn9a8b00717a2e")
-    //      .asString
-    //    println(nearbyCityRequest)
+  def getPlaces(city: City): List[Place] = {
+    // First query airport stations returned by skyscanner
+    val skyScannerPlaces = getSkyScannerPlaces(city)
 
-    // Had to execute curl process because of error "An error in one of the rendering components of OpenRasta prevents the error message from being sent back."
+    // Second query airport stations returned by flixbus api
+    // TODO
+
+    skyScannerPlaces // ++ flixBusPlaces
+  }
+
+  def getSkyScannerPlaces(city: City): List[Place] = {
+    /**
+    * Note: Had to execute curl process because of error:
+    *  "An error in one of the rendering components of OpenRasta prevents the error message from being sent back."
+    */
     val url = s"https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/${city.countryCode}/EUR/en-GB/?query=${city.name.replace(" ", "%20")}"
     val request = Process("curl", Seq("--request", "GET", "--url", url, "--header", "x-rapidapi-host:skyscanner-skyscanner-flight-search-v1.p.rapidapi.com", "--header", "x-rapidapi-key:c1d6dfff64msh11b436d0b19f66cp1aa76bjsn9a8b00717a2e"))
     val rawResponse = request.!!(ProcessLogger(_ => null))
@@ -179,13 +163,11 @@ object Search {
         val nearbyPlaces = nearbyPlacesRaw.map(place => {
           val placeId = place.getOrElse("PlaceId", "").toString
           val placeName = place.getOrElse("PlaceName", "").toString
-          Place(placeId, placeName, city.isTransit)
+          Place(placeId, placeName, TransportType.SkyScanner)
         })
         nearbyPlaces
       case None =>
-        println(request)
-        println(rawResponse)
-        println("Parsing failed")
+        println(s"Parsing failed - Request ${request} - Response: ${rawResponse}")
         List()
       case other =>
         println("Unknown data structure: " + other)
@@ -193,7 +175,13 @@ object Search {
     }
   }
 
-  case class City(name: String, countryCode: String, isTransit: Boolean ) {
+  /**
+   * City obtained from GeoDB close to some coordinates.
+   * @param name Name of the city.
+   * @param countryCode Universal country code where the city belongs.
+   * @param isTransit The distance of the city from the coordinates is lower than the first found + threshold.
+   */
+  case class City(name: String, countryCode: String, isTransit: Boolean, places: Set[Place]) {
     def canEqual(a: Any): Boolean = a.isInstanceOf[City]
 
     override def equals(that: Any): Boolean = that match {
@@ -201,22 +189,44 @@ object Search {
       case _ => false
     }
 
-    override def hashCode(): Int = {
-      name.hashCode
-    }
+    override def hashCode(): Int = name.hashCode
   }
 
-  case class Place(id: String, name: String, isTransit: Boolean) {
+  object TransportType extends Enumeration {
+    type TransportType = Value
+    val SkyScanner, FlixBus = Value
+  }
+
+  /**
+   * Places compatible with an API within a city
+   * @param id Unique id provided by the transport API (SkyScanner or Flixbus)
+   * @param name Name of the city provided by the transport API.
+   * @param transportType API used to obtained the place
+   */
+  case class Place(id: String, name: String, transportType: TransportType) {
+    def canEqual(a: Any): Boolean = a.isInstanceOf[Place]
+
     override def equals(that: Any): Boolean = that match {
       case that: Place => that.canEqual(this) && this.hashCode == that.hashCode
       case _ => false
     }
 
-    def canEqual(a: Any): Boolean = a.isInstanceOf[Place]
-
-    override def hashCode(): Int = {
-      id.hashCode
-    }
+    override def hashCode(): Int = id.hashCode
   }
 
+  /**
+   * Graph to go from origin to source
+   * @param origin Origin place
+   * @param destination Destination Place
+   * @param stops Possible stops
+   */
+  class RouteGraph(origin: City, destination: City, stops: Array[City]) {
+      def getRoutesBetweenCities(srcCity: City, destCity: City): Unit = {
+
+      }
+//    val edges = City ? Place?
+      def createGraph(): Unit = {
+        // Get possible edges between city and destination
+      }
+  }
 }
